6415e092d5136d6b5fe021d01e433f8ca96cd80c
Mebius <viddafroygeo@hotmail.com>
2018-10-17 20:07:39 +0200

    basic project

---
+++ LegoIos/LegoIos/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LegoIos
+//
+//  Created by Mebius on 17/10/2018.
+//  Copyright © 2018 Mebius. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
