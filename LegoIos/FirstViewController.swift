61df94377a50134d333c9bcbb0b62767d3fa4c44
Mebius <viddafroygeo@hotmail.com>
2019-01-01 12:51:09 +0100

    first HTTP call. Turns out, it's not that easy in swift

--- LegoIos/FirstViewController.swift
+++ LegoIos/FirstViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let authorization = "key=\(AppConfig.LEGO_API_KEY)"
+        
+        Alamofire.request("https://rebrickable.com/api/v3/lego/sets?page=last&page_size=10&ordering=year&\(authorization)").responseJSON { response in
+            if let result = response.result.value {
+                let json = JSON(result)
+                print(json)
+            }
+        }
     }
-
-
 }
 
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LegoIos' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LegoIos
+    pod 'Alamofire'
+    pod 'SwiftyJSON'
+
+  target 'LegoIosTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'LegoIosUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
