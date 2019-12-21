0c2dd57b02fd12aec854af3fd8ae29b5a52680b2
Mebius <viddafroygeo@hotmail.com>
2019-12-21 21:10:55 +0100

    Better fastlane + updated gitignore

--- fastlane/Fastlane.swift
+++ fastlane/Fastlane.swift
@@ -37,15 +37,6 @@ class Fastlane: XCTestCase {
         }
     }
 
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
-
     private func waitForElementToDisAppear(_ element: XCUIElement) -> Bool {
         let predicate = NSPredicate(format: "exists == false")
         let expectation = XCTNSPredicateExpectation(predicate: predicate,
