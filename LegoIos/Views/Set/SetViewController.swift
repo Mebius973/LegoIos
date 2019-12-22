ea05e5f8c174f296c113c25d17ffddafc61ec64a
Mebius <viddafroygeo@hotmail.com>
2019-12-22 20:21:48 +0100

    fix didSet setting a non existing element

--- LegoIos/Views/Set/SetViewController.swift
+++ LegoIos/Views/Set/SetViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class SetViewController: UIViewController, UISetCellDelegate {
-    @IBOutlet weak var mainLabel: UILabel!
-    private var _setCell: SetCell? {
+    @IBOutlet weak var mainLabel: UILabel! {
         didSet {
             if let num = _setCell!.set.setNum {
                 mainLabel.text = "Set Num: \(num)"
             }
         }
     }
+    private var _setCell: SetCell?
 
     override func viewDidLoad() {
         super.viewDidLoad()
