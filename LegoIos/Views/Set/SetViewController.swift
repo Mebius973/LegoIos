9651d0533b80fde1798f0f36675ac8ff3acded4a
Mebius <viddafroygeo@hotmail.com>
2019-12-24 12:01:53 +0100

    set view

--- LegoIos/Views/Set/SetViewController.swift
+++ LegoIos/Views/Set/SetViewController.swift
@@ -9,21 +9,41 @@
 import UIKit
 
 class SetViewController: UIViewController, UISetCellDelegate {
-    @IBOutlet weak var mainLabel: UILabel! {
-        didSet {
-            if let num = _setCell!.set.setNum {
-                mainLabel.text = "Set Num: \(num)"
-            }
-        }
-    }
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var setNameLabel: UILabel!
+    @IBOutlet weak var setYearLabel: UILabel!
+    @IBOutlet weak var setCategoryLabel: UILabel!
+    @IBOutlet weak var setPartsLabel: UILabel!
+    @IBOutlet weak var setSetNumLabel: UILabel!
+
     private var _setCell: SetCell?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUIElements()
     }
 
     func configure(with setCell: SetCell) {
         self._setCell = setCell
+    }
+
+    private func updateUIElements() {
+        if let setCell = _setCell {
+            image.image = setCell.image
+            setNameLabel.text = setCell.set.name
+            setNameLabel.adjustsFontSizeToFitWidth = true
+            if let year = setCell.set.year {
+                setYearLabel.text = String(year)
+            }
+            if let themeId = setCell.set.themeId {
+                setCategoryLabel.text = String(themeId)
+            }
+            if let numParts = setCell.set.numParts {
+                setPartsLabel.text = String(numParts)
+            }
+            setSetNumLabel.text = setCell.set.setNum
+        }
     }
 }
