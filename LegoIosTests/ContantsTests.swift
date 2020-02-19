52221e11adf690e567ca26486d6cb78af9ab89f4
Mebius <viddafroygeo@hotmail.com>
2020-02-19 08:51:12 +0100

    env selection tests

---
+++ LegoIosTests/ContantsTests.swift
@@ -0,0 +1,21 @@
+//
+//  ContantsTests.swift
+//  LegoIosTests
+//
+//  Created by Mebius on 19/02/2020.
+//  Copyright © 2020 Mebius. All rights reserved.
+//
+
+import XCTest
+@testable import LegoIos
+
+class ContantsTests: XCTestCase {
+    func testApiBaseURlWithoutArgsReturnsTrueURL() {
+        XCTAssert(Constants.ApiBaseURL.contains("rebrickable"))
+    }
+
+    func testApiBaseURlWithArgsReturnsLocalURL() {
+        CommandLine.arguments.append("-runlocal")
+        XCTAssert(Constants.ApiBaseURL.contains("localhost"))
+    }
+}
--- LegoIosTests/SetPartsViewModelTests.swift
+++ LegoIosTests/SetPartsViewModelTests.swift
@@ -12,16 +12,12 @@ import XCTest
 class SetPartsViewModelTests: XCTestCase {
     private let viewModel = SetPartsViewModel()
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssert(viewModel.count == 0)
     }
 
